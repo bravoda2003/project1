8644cf22eca7a87fb61ec8df97fc937a9aa1e3ce
bravoda2003 <2203031050183@paruluniversity.ac.in>
2024-02-22 11:07:39 +0530

    this is our commit files

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+import mysql.connector
+mydb=mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password='Bravoda@2003'
+)
+myc = mydb.cursor()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+myc.execute("CREATE DATABASE emanual")
 
+mydb = msql.connector.connect(
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+)
